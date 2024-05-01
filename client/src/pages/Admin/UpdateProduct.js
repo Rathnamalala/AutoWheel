151034da9e9f9a65b01708ca6df2198d0aefa52c
Prabath Rathnamalala <prabathrathnamalala2k2@gmail.com>
2024-05-01 21:30:21 +0530

    update product

--- client/src/pages/Admin/UpdateProduct.js
+++ client/src/pages/Admin/UpdateProduct.js
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { Select } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
+import { set } from "mongoose";
 const { Option } = Select;
 
 const UpdateProduct = () => {
@@ -12,10 +13,18 @@ const UpdateProduct = () => {
   const params = useParams();
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
+  const [model, setModel] = useState("");
+  const [year, setYear] = useState("");
+  const [mileage, setMileage] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
+  const [transmission,setTransmission] = useState("");
+  const [description,setdescription] = useState("");
+  const [color,setColor] = useState("");
+  const [condition,setCondition] = useState("");
   const [photo, setPhoto] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
   const [id, setId] = useState("");
 
   //get single product
@@ -26,9 +35,19 @@ const UpdateProduct = () => {
       );
       setName(data.car.name);
       setId(data.car._id);
-      setDescription(data.car.description);
+      setdescription(data.car.description);
       setPrice(data.car.price);
       setPrice(data.car.price);
+      setPhoto(data.car.photo);
+      setModel(data.car.model);
+      setYear(data.car.year);
+      setMileage(data.car.mileage);
+      setTransmission(data.car.transmission);
+      setColor(data.car.color);
+      setCondition(data.car.condition);
+      setPhone(data.car.phone);
+      setAddress(data.car.address);
+
      
       
       setCategory(data.car.category._id);
@@ -61,16 +80,23 @@ const UpdateProduct = () => {
   const handleUpdate = async (e) => {
     e.preventDefault();
     try {
-      const productData = new FormData();
-      productData.append("name", name);
-      productData.append("description", description);
-      productData.append("price", price);
-     
-      photo && productData.append("photo", photo);
-      productData.append("category", category);
+      const carData = new FormData();
+      carData.append("name", name);
+      carData.append("description", description);
+      carData.append("price", price);
+      carData.append("model", model);
+      carData.append("year", year);
+      carData.append("mileage", mileage);
+      carData.append("transmission", transmission);
+      carData.append("color", color);
+      carData.append("condition", condition);
+      carData.append("phone", phone);
+      carData.append("address", address);
+      photo && carData.append("photo", photo);
+      carData.append("category", category);
       const { data } = axios.put(
         `/api/v1/car/update-car/${id}`,
-        productData
+        carData
       );
       if (data?.success) {
         toast.error(data?.message);
@@ -174,7 +200,7 @@ const UpdateProduct = () => {
                   value={description}
                   placeholder="write a description"
                   className="form-control"
-                  onChange={(e) => setDescription(e.target.value)}
+                  onChange={(e) => setdescription(e.target.value)}
                 />
               </div>
 
@@ -187,6 +213,79 @@ const UpdateProduct = () => {
                   onChange={(e) => setPrice(e.target.value)}
                 />
               </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={model}
+                  placeholder="write a model"
+                  className="form-control"
+                  onChange={(e) => setModel(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={year}
+                  placeholder="write a year"
+                  className="form-control"
+                  onChange={(e) => setYear(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="number"
+                  value={mileage}
+                  placeholder="write a mileage"
+                  className="form-control"
+                  onChange={(e) => setMileage(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={transmission}
+                  placeholder="write a transmission"
+                  className="form-control"
+                  onChange={(e) => setTransmission(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={color}
+                  placeholder="write a color"
+                  className="form-control"
+                  onChange={(e) => setColor(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={condition}
+                  placeholder="write a condition"
+                  className="form-control"
+                  onChange={(e) => setCondition(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={phone}
+                  placeholder="write a phone"
+                  className="form-control"
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="text"
+                  value={address}
+                  placeholder="write a address"
+                  className="form-control"
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </div>
+              
               
               
               <div className="mb-3">
